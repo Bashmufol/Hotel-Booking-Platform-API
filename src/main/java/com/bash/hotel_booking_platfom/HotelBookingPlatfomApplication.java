cf7c461977b53dfcab4e238200d39d1b36d93fc0
Muhammed Bashir <bashmufol@gmail.com>
2025-09-08 15:58:15 +0100

    my first commit

---
+++ src/main/java/com/bash/hotel_booking_platfom/HotelBookingPlatfomApplication.java
@@ -0,0 +1,13 @@
+package com.bash.hotel_booking_platfom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingPlatfomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingPlatfomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bash/hotel_booking_platfom/HotelBookingPlatfomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.hotel_booking_platfom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingPlatfomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
