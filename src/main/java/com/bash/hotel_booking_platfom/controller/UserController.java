d68b7f8d1d2c657dc6950371e8d5efa5650505de
Muhammed Bashir <bashmufol@gmail.com>
2025-09-09 11:05:33 +0100

    task 1 controller updated

--- src/main/java/com/bash/hotel_booking_platfom/controller/UserController.java
+++ src/main/java/com/bash/hotel_booking_platfom/controller/UserController.java
@@ -1,29 +1,26 @@
 package com.bash.hotel_booking_platfom.controller;
 
-import com.bash.hotel_booking_platfom.Repository.UserRepository;
+
 import com.bash.hotel_booking_platfom.model.User;
 import com.bash.hotel_booking_platfom.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/create")
-    public User createUser(User user) {
+    public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
     @PostMapping("/update/{id}")
-    public User updateUser(@PathVariable UUID id, User user) {
+    public User updateUser(@PathVariable UUID id, @RequestBody User user) {
         return userService.updateUser(id, user);
     }
 
