487f7febe8753990955910c68eb7262a64839b73
Muhammed Bashir <bashmufol@gmail.com>
2025-09-10 18:23:55 +0100

    updated

--- src/main/java/com/bash/hotel_booking_platfom/model/User.java
+++ src/main/java/com/bash/hotel_booking_platfom/model/User.java
@@ -1,19 +1,20 @@
 package com.bash.hotel_booking_platfom.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.UUID;
 @Data
+@Table(name = "users")
+@Entity
 public class User {
+    @Id
     private UUID id;
     private String name;
     private String email;
     private int age;
 
-//    public User(String name, String email, int age) {
-//        this.name = name;
-//        this.email = email;
-//        this.age = age;
-//    }
 }
