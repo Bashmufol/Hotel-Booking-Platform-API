b91320d8a0349160186b63f2ffe666d6741993cb
Muhammed Bashir <bashmufol@gmail.com>
2025-09-11 07:48:37 +0100

    updated

--- src/main/java/com/bash/hotel_booking_platfom/security/JwtAuthFilter.java
+++ src/main/java/com/bash/hotel_booking_platfom/security/JwtAuthFilter.java
@@ -1,4 +1,45 @@
 package com.bash.hotel_booking_platfom.security;
 
-public class JwtAuthFilter e{
+import com.bash.hotel_booking_platfom.service.CustomUserDetailsService;
+import com.bash.hotel_booking_platfom.service.UserPrincipal;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        String jwt = null;
+        String username = null;
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring("Bearer ".length()).trim();
+            username = jwtService.extractUsername(jwt);
+        }
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserPrincipal userPrincipal = (UserPrincipal) this.customUserDetailsService.loadUserByUsername(username);
+
+            if(jwtService.isTokenValid(jwt, userPrincipal)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userPrincipal, null, userPrincipal.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/com/bash/hotel_booking_platfom/security/JwtService.java
+++ src/main/java/com/bash/hotel_booking_platfom/security/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
@@ -14,13 +15,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
     @Value("${jwt.expiration.time}")
     private long exprirationTime;
     @Value("${jwt.secret.key}")
     private String secretKey;
 
-    private String extractUsername(String token) {
+    public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
     private Date extractExpiration(String token) {
@@ -29,7 +31,7 @@ public class JwtService {
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
-    private boolean isTokenValid(String token, UserPrincipal user) {
+    public boolean isTokenValid(String token, UserPrincipal user) {
         final String username = extractUsername(token);
         return (username.equals(user.getUsername()) && !isTokenExpired(token));
     }
