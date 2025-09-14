ba2d86a8d781437a2e3b7a0ceed486456c5f35f6
Muhammed Bashir <bashmufol@gmail.com>
2025-09-14 17:20:22 +0100

    updated

--- src/main/java/com/bash/hotel_booking_platfom/controller/RoomController.java
+++ src/main/java/com/bash/hotel_booking_platfom/controller/RoomController.java
@@ -1,10 +1,13 @@
 package com.bash.hotel_booking_platfom.controller;
 
+import com.bash.hotel_booking_platfom.dto.CreateRoomRequest;
+import com.bash.hotel_booking_platfom.model.Room;
 import com.bash.hotel_booking_platfom.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/room")
@@ -13,5 +16,26 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping("/create")
+    public Room createRoom(@RequestBody CreateRoomRequest request) {
+        return roomService.createRoom(request);
+    }
+
+    @GetMapping("/available")
+    public List<Room> searchAvailableRooms(
+            @RequestParam LocalDate checkIn,
+            @RequestParam LocalDate checkOut) {
+        return roomService.searchAvailableRooms(checkIn, checkOut);
+    }
+
+    @GetMapping("/{id}")
+    public Room getRoom(@PathVariable long id) {
+        return roomService.findRoomById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteRoom(@PathVariable long id) {
+        return roomService.deleteRoomById(id);
+    }
+
 
 }
