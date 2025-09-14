f148feba60dffa3d0d866e874b39eb0a3ba89b5e
Muhammed Bashir <bashmufol@gmail.com>
2025-09-14 11:54:37 +0100

    updated

--- src/main/java/com/bash/hotel_booking_platfom/controller/UserController.java
+++ src/main/java/com/bash/hotel_booking_platfom/controller/UserController.java
@@ -3,6 +3,8 @@ package com.bash.hotel_booking_platfom.controller;
 
 import com.bash.hotel_booking_platfom.dto.LoginRequest;
 import com.bash.hotel_booking_platfom.dto.RegisterRequest;
+import com.bash.hotel_booking_platfom.dto.UserDto;
+import com.bash.hotel_booking_platfom.model.ResponseModel;
 import com.bash.hotel_booking_platfom.model.User;
 import com.bash.hotel_booking_platfom.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -17,31 +19,35 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public User createUser(@RequestBody RegisterRequest registerRequest) {
+    public ResponseModel<UserDto> createUser(@RequestBody RegisterRequest registerRequest) {
         return userService.createUser(registerRequest);
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public ResponseModel<String> login(@RequestBody LoginRequest loginRequest) {
        return userService.login(loginRequest);
     }
     @PostMapping("/update/{id}")
-    public User updateUser(@PathVariable UUID id, @RequestBody User user) {
+    public ResponseModel<UserDto> updateUser(@PathVariable UUID id, @RequestBody User user) {
         return userService.updateUser(id, user);
     }
 
     @GetMapping("/get-user/{id}")
-    public User findById(@PathVariable UUID id) {
+    public ResponseModel<UserDto> findById(@PathVariable UUID id) {
+
         return userService.findUserById(id);
     }
+
     @GetMapping("/users")
     public List<User> findAll() {
         return userService.findAllUsers();
     }
+
     @GetMapping("/delete-user/{id}")
     public String deleteUser(@PathVariable UUID id) {
         return userService.deleteUser(id);
     }
+
     @GetMapping("/delete-all")
     public String deleteAllUsers() {
         return userService.deleteAllUsers();
