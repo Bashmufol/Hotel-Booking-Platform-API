9639196279370e6e3307c0dbe285ff4e0f0287a6
Muhammed Bashir <bashmufol@gmail.com>
2025-09-16 04:59:49 +0100

    updated

--- src/main/java/com/bash/hotel_booking_platfom/controller/UserController.java
+++ src/main/java/com/bash/hotel_booking_platfom/controller/UserController.java
@@ -54,5 +54,5 @@ public class UserController {
     public String deleteAllUsers() {
         return userService.deleteAllUsers();
     }
-dghhsdhsaghsdvcshvshvhvsavschgbxbsbdvanmgdhdgsbbdbd
+dghhsdhsaghsdvcshvshvhvsavschgbxbsbdvanmgdhdgs
 }
